8d3c64a2c619e5f19a35e426468e1201e8c4907c
yeoularu <yeoularu@gmail.com>
2022-12-08 14:45:16 +0900

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+print("edit <code>app.py</code> and reload this page.")
+
+# plot
+x = np.random.randn(100)
+y = np.random.randn(100)
+fig, ax = plt.subplots()
+fig.patch.set_facecolor('#2D2E35')
+ax.patch.set_facecolor('#2D2E35')
+ax.scatter(x, y)
+ax.text(x=0, y=0, s="<py>", c="orange")
+fig
