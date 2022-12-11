5990f244e96ccca8343666a46306f4eebf11260c
yeoularu <yeoularu@gmail.com>
2022-12-11 18:16:50 +0900

    version up to 0.0.2

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ y = np.random.randn(100)
 fig, ax = plt.subplots()
 fig.patch.set_facecolor('#2D2E35')
 ax.patch.set_facecolor('#2D2E35')
-ax.scatter(x, y)
+ax.scatter(x, y)    # TRY putting ax.step(x, y) in here instead.
 ax.text(x=0, y=0, s="<py>", c="orange")
 fig
